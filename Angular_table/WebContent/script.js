9cfa30766c35ae01faaa9fc10ce49864057efbfc
Krishna Chandu <Krishna Chandu@ACSPLDEV045>
2019-05-27 11:59:10 +0530

    27/05/2019

--- Angular_table/WebContent/script.js
+++ Angular_table/WebContent/script.js
@@ -3,9 +3,9 @@ var app = angular.module('plunker', []);
 app.controller('MainCtrl', function($scope) {
     
     $scope.items = [
-        {name: 'datta', sub1: 65, sub2: 70, sub3: 80 },    
-        {name: 'hemasai', sub1: 53, sub2: 68, sub3:78},
-        {name: 'kishore', sub1: 73, sub2: 81, sub3: 89}
+        {name: 'Datta', sub1: 60, sub2: 70, sub3: 80 },    
+        {name: 'Hemasai', sub1: 53, sub2: 68, sub3:78},
+        {name: 'Kishore', sub1: 73, sub2: 81, sub3: 89}
     ];
     
     $scope.getTotal = function(type) {
@@ -15,4 +15,6 @@ app.controller('MainCtrl', function($scope) {
         });
         return total;
     };
+    
+    
 });
